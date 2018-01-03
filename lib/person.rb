bf9cc1b19d65bee7146731b5c88cbb437faeb478
iwilliams83 <indirajamurath@gmail.com>
2018-01-03 16:10:11 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,28 @@
 class Person
   #your code here
+  ATTRIBUTES = {
+    :id => "INTEGER PRIMARY KEY",
+    :name => "TEXT",
+    :birthday => "TEXT",
+    :hair_color =>  "TEXT",
+    :eye_color =>  "TEXT",
+    :height =>  "TEXT",
+    :weight  => "TEXT",
+    :handed =>  "TEXT",
+    :complexion => "TEXT",
+    :t_shirt_size => "TEXT", 
+    :wrist_size => "TEXT", 
+    :glove_size => "TEXT",
+    :pant_length => "TEXT",
+    :pant_width => "TEXT"
+  }
+  # ATTRIBUTES.keys.each do |attribute|
+  #   attr_accessor attribute
+  # end
+  attr_accessor *ATTRIBUTES.keys
+  
+  def initialize(attribute_hash)
+    attribute_hash.map { |(k, v)| send("#{k}=", v) }
+  end
+ 
 end
